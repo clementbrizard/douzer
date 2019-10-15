b2ace377f8a300a2dd01d93cdf2d1e4a183d745c
Antoine Pilon <antoine.pilon@etu.utc.fr>
2019-10-15 22:01:23 +0200

    feat: added Server class

---
+++ network/src/main/java/threads/Server.java
@@ -0,0 +1,32 @@
+package threads;
+
+import java.net.ServerSocket;
+
+import provider.NetworkProvider;
+
+/**
+ * Server class running in a different thread. It listens to the default
+ * port and will ask network provider to process messages in another thread when
+ * a message is received.
+ * 
+ * @author Antoine
+ */
+public class Server extends Thread {
+
+	private ServerSocket listeningSocket;
+	private NetworkProvider netProvider;
+	
+	public Server(NetworkProvider n) {
+		this.netProvider = n;
+	}
+	
+	public void kill() {
+        //To be define
+    }
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+}
