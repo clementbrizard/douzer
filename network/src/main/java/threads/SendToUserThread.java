70954826944f6063c123aa2c932e5a94c18aed50
Alexandre Touzeau <alexandre.touzeau@etu.utc.fr>
2019-10-15 19:19:17 +0200

    Add class SendToUsersThread SendToUsersThread ThreadExtend

---
+++ network/src/main/java/threads/SendToUserThread.java
@@ -0,0 +1,24 @@
+package threads;
+
+import message.Message;
+
+import java.net.InetAddress;
+
+public class SendToUserThread extends ThreadExtend {
+    private Payload p;
+    private InetAddress ip;
+
+    public SendToUserThread(Payload p){
+        //To be define
+    }
+
+    @Override
+    public void run() {
+        //To be define
+    }
+
+    @Override
+    public void kill() {
+        //To be define
+    }
+}
---
+++ network/src/main/java/threads/SendToUsersThread.java
@@ -0,0 +1,25 @@
+package threads;
+
+import java.net.InetAddress;
+import java.util.stream.Stream;
+
+public class SendToUsersThread extends ThreadExtend {
+
+    private Payload payload;
+    private Stream<InetAddress> usersIps;
+
+    public SendToUsersThread(Payload p, Stream<InetAddress> s){
+        this.payload = p;
+        this.usersIps = s;
+    }
+
+    @Override
+    public void run() {
+        //To be define
+    }
+
+    @Override
+    public void kill() {
+        //To be define
+    }
+}
