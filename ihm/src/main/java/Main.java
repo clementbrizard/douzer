c7e49f8bdab93bdec1f91c61c1786c8accd96288
kchartra <kyllian.chartrain@etu.utc.fr>
2019-10-16 02:17:59 +0200

    Feat : added dependency in maven a main a test view and a test controller
    for all ihm to know if javafx it's installed with maven and can run the test application with simple popUp message

---
+++ ihm/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.awt.Toolkit;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	
+	double witdh = Toolkit.getDefaultToolkit().getScreenSize().getWidth()/2;
+	double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2;
+	
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/views/TestView.fxml"));
+        primaryStage.setTitle("first test");
+        primaryStage.setScene(new Scene(root,witdh , height));
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+
+
+    
+    
+    public static void main(String[] args) {
+    	System.out.println("start application");
+        launch(args);
+    }
+}
---
+++ ihm/src/main/java/controllers/TestController.java
@@ -0,0 +1,90 @@
+package controllers;
+
+import java.util.Random;
+
+import org.controlsfx.control.Notifications;
+
+import javafx.application.Platform;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class TestController {
+	
+	@FXML
+	private Label label;
+	
+    @FXML
+    private Button buttonClick;
+	
+	static int count = 0 ;
+	
+    public void initialize() {
+    	
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        
+        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+    }
+	
+    
+    
+	public Label getLabel() {
+		return label;
+	}
+	@FXML
+    private void test(ActionEvent event)
+    {        
+    	count++;
+        launchThread(count);
+        buttonClick.setText("click " + count + " time");
+        label.setText("appuyé");        
+    }
+    
+    
+    
+    private void launchThread(int countCopy) {
+
+        Random rand = new Random();
+    	int[] n = {rand.nextInt(5)};
+        n[0]++;
+        
+        
+        //demarrage d'un service 
+    	Service<Void> service = new Service<Void>(){
+    		@Override
+    		protected Task<Void> createTask(){
+    			return new Task<Void>(){
+    				
+    				@Override
+    				protected Void call() throws Exception{
+    	                System.out.println("dans le thread");
+    	                Thread.sleep(n[0]*1000);
+    	                System.out.println("après " + n[0] + " secondes");
+    	                
+    	                //la fonction runLater qui permet de lancer la notification dans le thread 
+    	                Platform.runLater(()->{
+    	                	//une notification
+	    	                Notifications.create()
+	    	                .title("Title Text")
+	    	                .text("pop up n° " + countCopy + " après  : " + n[0] +  " secondes")
+	    	                .darkStyle()
+	    	                .position(Pos.TOP_LEFT)
+	    	                .showWarning();
+	    	                });
+    	                count++;
+    	                System.out.println("après la notification");
+
+    	              return null;
+    				}
+    			};
+    		}
+    	};
+    	service.start();
+    
+    }
+}
