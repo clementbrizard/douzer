02dab57e342e6482da6b1295fd6fd2570b3e0660
Antoine Pilon <antoine.pilon@etu.utc.fr>
2019-10-21 16:46:35 +0200

    feat: added class NetworkImpl

---
+++ network/src/main/java/networkImplementation/NetworkImpl.java
@@ -0,0 +1,28 @@
+package networkImplementation;
+
+import java.net.InetAddress;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public class NetworkImpl implements Net {
+
+  public void sendToUser(String payload, InetAddress ipDets) {
+    return;
+  }
+  
+  public void sendToUsers(String payload, Stream<InetAddress> ipsDest) {
+    return;
+  }
+  
+  public void requestDownload(Stream<InetAddress> sourcesIPs, String musicHash) {
+    return;
+  }
+  
+  public void connect(String payload, Collection<InetAddress> knownIPs) {
+    return;
+  }
+  
+  public void disconnect(String payload, Collection<InetAddress> knownIPs) {
+    return;
+  }
+}
