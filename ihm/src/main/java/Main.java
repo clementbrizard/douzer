3a7d6891987971f2006b64f359f8e4f62db3573a
pguilbau <perrine.guilbaud@etu.utc.fr>
2019-10-22 22:00:10 +0200

    Feature: start ihm application from ihm core instead of from the main

--- ihm/src/main/java/Main.java
+++ ihm/src/main/java/Main.java
@@ -1,27 +1,12 @@
-import java.awt.Toolkit;
+import core.IhmCore;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+public class Main {
 
-public class Main extends Application {
-  
-  double witdh = Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 2;
-  double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 2;
-  
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("/views/TestView.fxml"));
-    primaryStage.setTitle("first test");
-    primaryStage.setScene(new Scene(root,witdh,height));
-    primaryStage.setMaximized(true);
-    primaryStage.show();
-  }
+	public static void main(String[] args) {
+		// start data
+		// start network
+		IhmCore Core = new IhmCore();
+		Core.run(args);
+	}
 
-  public static void main(String[] args) {
-    System.out.println("start application");
-    launch(args);
-  }
 }
--- ihm/src/main/java/core/IhmCore.java
+++ ihm/src/main/java/core/IhmCore.java
@@ -4,16 +4,27 @@ import controllers.LoginController;
 import controllers.MainController;
 import controllers.SignUpController;
 
+import java.awt.Toolkit;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 //replace by javadocs
 //start the FX application
-public class IhmCore {
-  
+public class IhmCore extends Application {
+
   private MainController mainController;
   private LoginController loginController;
   private SignUpController signUpController;
-  
+
+  double witdh = Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 2;
+  double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 2;
+
   public IhmCore() {}
-  
+
   public void setMainController(MainController mainController) {
     this.mainController = mainController;
   }
@@ -21,20 +32,35 @@ public class IhmCore {
   public void setLoginController(LoginController loginController) {
     this.loginController = loginController;
   }
-  
+
   public void setSignUpController(SignUpController signUpController) {
     this.signUpController = signUpController;
   }
-  
+
   public MainController getMainController() {
     return this.mainController;
   }
-  
+
   public LoginController getLoginController() {
     return this.loginController;
   }
-  
+
   public SignUpController getSignUpController() {
     return this.signUpController;
   }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("/views/TestView.fxml"));
+    primaryStage.setTitle("first test");
+    primaryStage.setScene(new Scene(root,witdh,height));
+    primaryStage.setMaximized(true);
+    primaryStage.show();
+  }
+
+  public void run(String[] args) {
+	  launch(args); 
+  }
+
+
 }
