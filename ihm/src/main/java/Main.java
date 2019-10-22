4adef8ee649edd9ee25b10921951791281b0ddd5
Pauline <pauline.guillet_02@etu.utc.fr>
2019-10-22 21:30:15 +0200

    style : Side screens and MyMusics center views

--- ihm/src/main/java/Main.java
+++ ihm/src/main/java/Main.java
@@ -6,10 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main {
-  /**
-  * start application main.
-  */
+public class Main extends Application {
+  
+  double witdh = Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 2;
+  double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 2;
+  
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("/views/ContactsView.fxml"));
+    primaryStage.setTitle("first test");
+    primaryStage.setScene(new Scene(root,witdh,height));
+    primaryStage.setMaximized(true);
+    primaryStage.show();
+  }
+
   public static void main(String[] args) {
     //start data
     //start network
