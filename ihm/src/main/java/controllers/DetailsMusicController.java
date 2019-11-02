e360f2c9264e5cfdd7e6a0329c135b8ef3d7b6c5
kchartra <kyllian.chartrain@etu.utc.fr>
2019-11-02 11:05:08 +0100

    Feature : add the DetailsMusicController

--- ihm/src/main/java/controllers/DetailsMusicController.java
+++ ihm/src/main/java/controllers/DetailsMusicController.java
@@ -1,21 +1,71 @@
 package controllers;
 
+import datamodel.LocalMusic;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 //replace by javadocs
 //central view to show all information about on music
 public class DetailsMusicController implements Controller {
 
-  private CentralFrameController centralFrameController;
+  private MyMusicsController myMusicsController;
+  
+  public MyMusicsController getMyMusicsController() {
+    return this.myMusicsController;
+  }
+  
+  public void setMyMusicsController(MyMusicsController myMusicsController) {
+    this.myMusicsController = myMusicsController;
+  }
+  
+  /**
+   * After the initialisation of the controller execute this function with
+   * the localMusic where the user has click
+   * @param localMusic
+   */
+  public void initMusic(LocalMusic localMusic) {
+    
+    if(localMusic.getMetadata() != null)
+      if(localMusic.getMetadata().getTitle() != null)
+          textFieldTitre.setText(localMusic.getMetadata().getTitle());
+    
+    if(localMusic.getMetadata() != null)
+      if(localMusic.getMetadata().getArtist() != null)
+          textFieldArtiste.setText(localMusic.getMetadata().getArtist());
+    
+    if(localMusic.getMetadata() != null)
+      if(localMusic.getMetadata().getAlbum() != null)
+          textFieldAlbum.setText(localMusic.getMetadata().getAlbum());
+    
+    if(localMusic.getMetadata() != null)
+      if(localMusic.getMetadata().getReleaseDate() != null)
+          textFieldAnnee.setText("" + localMusic.getMetadata().getReleaseDate().getYear());
+    
+    if(localMusic.getOwners() != null)
+      if(localMusic.getOwners().iterator().hasNext())
+        textFieldLastUploader.setText(localMusic.getOwners().iterator().next().getUsername());
+  }
 
+  @FXML
+  private TextField textFieldTitre;
+  
+  @FXML
+  private TextField textFieldArtiste;
+  
+  @FXML
+  private TextField textFieldAlbum;
+  
+  @FXML
+  private TextField textFieldAnnee;
+  
+  @FXML
+  private TextField textFieldLastUploader;
+  
+  
   @Override
   public void initialize() {
-    // TODO Auto-generated method stub
+    
   }
 
-  public CentralFrameController getCentralFrameController() {
-    return centralFrameController;
-  }
-
-  public void setCentralFrameController(CentralFrameController centralFrameController) {
-    this.centralFrameController = centralFrameController;
-  }
+  
 }
