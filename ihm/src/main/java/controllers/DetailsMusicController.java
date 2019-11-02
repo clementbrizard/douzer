3fb09e40ccba928106fb16139b6bc67826012292
pguilbau <perrine.guilbaud@etu.utc.fr>
2019-11-02 12:38:10 +0100

    feature: add tag responsiveness

--- ihm/src/main/java/controllers/DetailsMusicController.java
+++ ihm/src/main/java/controllers/DetailsMusicController.java
@@ -1,10 +1,12 @@
 package controllers;
 
 import datamodel.LocalMusic;
+import java.util.Iterator;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -16,103 +18,114 @@ import javafx.scene.input.MouseEvent;
 public class DetailsMusicController implements Controller {
 
   private MyMusicsController myMusicsController;
-  
+
   public MyMusicsController getMyMusicsController() {
     return this.myMusicsController;
   }
-  
+
   public void setMyMusicsController(MyMusicsController myMusicsController) {
     this.myMusicsController = myMusicsController;
   }
-  
+
   /**
    * After the initialisation with initialize() of the controller execute this function with
    * the localMusic where the user has click .
    * @param localMusic the music clicked
    */
   public void initMusic(LocalMusic localMusic) {
-    
+
     if (localMusic.getMetadata() != null) {
       if (localMusic.getMetadata().getTitle() != null) {
         textFieldTitre.setText(localMusic.getMetadata().getTitle());
       }
     }
-    
+
     if (localMusic.getMetadata() != null) {
       if (localMusic.getMetadata().getArtist() != null) {
         textFieldArtiste.setText(localMusic.getMetadata().getArtist());
       }
     }
-    
+
     if (localMusic.getMetadata() != null) {
       if (localMusic.getMetadata().getAlbum() != null) {
         textFieldAlbum.setText(localMusic.getMetadata().getAlbum());
       }
     }
-    
+
     if (localMusic.getMetadata() != null) {
       if (localMusic.getMetadata().getReleaseDate() != null) {
         textFieldAnnee.setText("" + localMusic.getMetadata().getReleaseDate().getYear());
       }
     }
-    
+
     if (localMusic.getOwners() != null) {
       if (localMusic.getOwners().iterator().hasNext()) {
         textFieldLastUploader.setText(localMusic.getOwners().iterator().next().getUsername());
       }
     }
-    
+
     tags = FXCollections.observableArrayList();
-    
+
     if (localMusic.getMetadata() != null) {
       if (localMusic.getMetadata().getTags() != null) {
-        
+
         while (localMusic.getMetadata().getTags().iterator().hasNext()) {
           tags.add(localMusic.getMetadata().getTags().iterator().next());
         }
-        
+
         listViewTagsList.setItems(tags);
       }
     }
-    
-    
+
+
   }
 
   @FXML
   private TextField textFieldTitre;
-  
+
   @FXML
   private TextField textFieldArtiste;
-  
+
   @FXML
   private TextField textFieldAlbum;
-  
+
   @FXML
   private TextField textFieldAnnee;
-  
+
   @FXML
   private TextField textFieldLastUploader;
-  
+
+  @FXML
+  private TextField textFieldAddTag;
+
   @FXML
   private ListView<String> listViewTagsList;
-  
+
   private ObservableList<String> tags;
-  
+
+  private LocalMusic localMusic;
+
+  @FXML
+  private Button buttonValider;
+
+  @FXML
+  private Button buttonAddTag;
+
   @FXML
   private ImageView imageEtoile1;
-  
+
   @FXML
   private ImageView imageEtoile2;
-  
+
   @FXML
   private ImageView imageEtoile3;
-  
+
   @FXML
   private ImageView imageEtoile4;
-  
+
   @FXML
   private ImageView imageEtoile5;
-  
+
   @Override
   public void initialize() {
     imageEtoile1.setOnMousePressed((new EventHandler<MouseEvent>() {
@@ -120,13 +133,37 @@ public class DetailsMusicController implements Controller {
       @Override
       public void handle(MouseEvent event) {
         System.out.println("clique sur l'étoile 1");
-        imageEtoile1.setImage(new Image("my/resources/images/FullStarSymbol.png") );
-      } 
-      
+        imageEtoile1.setImage(new Image("my/resources/images/FullStarSymbol.png"));
+      }
+
+
+
     }));
-    
-    
+
+    buttonAddTag.setOnMousePressed((new EventHandler<MouseEvent>() {
+
+        @Override
+        public void handle(MouseEvent event) {
+          System.out.println("clique sur add tag");
+          boolean tagexist = false;
+          if (localMusic.getMetadata() != null) {
+            if (localMusic.getMetadata().getTags() != null) {
+              Iterator<String> itMusicTag = localMusic.getMetadata().getTags().iterator();
+              while (itMusicTag.hasNext()) {
+                if (itMusicTag.next().equals(textFieldAddTag.getText())) {
+                  tagexist = true;
+                }
+              }
+            }
+          }
+          if (!tagexist) {
+            tags.add(textFieldAddTag.getText());
+          }
+          //give new tag or new tag list to data
+        }
+    }));
+
   }
 
-  
+
 }
