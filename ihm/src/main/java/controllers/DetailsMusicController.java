079c786a82c865d005c379e12d8147c167f3188d
kchartra <kyllian.chartrain@etu.utc.fr>
2019-11-02 11:28:08 +0100

    fix : checkstyle

--- ihm/src/main/java/controllers/DetailsMusicController.java
+++ ihm/src/main/java/controllers/DetailsMusicController.java
@@ -23,42 +23,53 @@ public class DetailsMusicController implements Controller {
   
   /**
    * After the initialisation with initialize() of the controller execute this function with
-   * the localMusic where the user has click
-   * @param localMusic
+   * the localMusic where the user has click .
+   * @param localMusic the music clicked
    */
   public void initMusic(LocalMusic localMusic) {
     
-    if(localMusic.getMetadata() != null)
-      if(localMusic.getMetadata().getTitle() != null)
-          textFieldTitre.setText(localMusic.getMetadata().getTitle());
+    if (localMusic.getMetadata() != null) {
+      if (localMusic.getMetadata().getTitle() != null) {
+        textFieldTitre.setText(localMusic.getMetadata().getTitle());
+      }
+    }
     
-    if(localMusic.getMetadata() != null)
-      if(localMusic.getMetadata().getArtist() != null)
-          textFieldArtiste.setText(localMusic.getMetadata().getArtist());
+    if (localMusic.getMetadata() != null) {
+      if (localMusic.getMetadata().getArtist() != null) {
+        textFieldArtiste.setText(localMusic.getMetadata().getArtist());
+      }
+    }
     
-    if(localMusic.getMetadata() != null)
-      if(localMusic.getMetadata().getAlbum() != null)
-          textFieldAlbum.setText(localMusic.getMetadata().getAlbum());
+    if (localMusic.getMetadata() != null) {
+      if (localMusic.getMetadata().getAlbum() != null) {
+        textFieldAlbum.setText(localMusic.getMetadata().getAlbum());
+      }
+    }
     
-    if(localMusic.getMetadata() != null)
-      if(localMusic.getMetadata().getReleaseDate() != null)
-          textFieldAnnee.setText("" + localMusic.getMetadata().getReleaseDate().getYear());
+    if (localMusic.getMetadata() != null) {
+      if (localMusic.getMetadata().getReleaseDate() != null) {
+        textFieldAnnee.setText("" + localMusic.getMetadata().getReleaseDate().getYear());
+      }
+    }
     
-    if(localMusic.getOwners() != null)
-      if(localMusic.getOwners().iterator().hasNext())
+    if (localMusic.getOwners() != null) {
+      if (localMusic.getOwners().iterator().hasNext()) {
         textFieldLastUploader.setText(localMusic.getOwners().iterator().next().getUsername());
+      }
+    }
     
     tags = FXCollections.observableArrayList();
     
-    if(localMusic.getMetadata() != null)
-      if(localMusic.getMetadata().getTags() != null) {
+    if (localMusic.getMetadata() != null) {
+      if (localMusic.getMetadata().getTags() != null) {
         
-        while(localMusic.getMetadata().getTags().iterator().hasNext()) {
+        while (localMusic.getMetadata().getTags().iterator().hasNext()) {
           tags.add(localMusic.getMetadata().getTags().iterator().next());
         }
         
         listViewTagsList.setItems(tags);
       }
+    }
     
     
   }
