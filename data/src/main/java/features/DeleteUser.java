ba4f7a6e8f197b442b53c37b75f12d10bb6c593d
Alexandre Touzeau <alexandre.touzeau@etu.utc.fr>
2019-11-11 12:18:53 +0100

    Add function to delete user in lo23-users.ser file

--- data/src/main/java/features/DeleteUser.java
+++ data/src/main/java/features/DeleteUser.java
@@ -1,77 +1,84 @@
 package features;
 
 import core.Datacore;
+import datamodel.LocalUser;
 
+import java.io.EOFException;
 import java.io.File;
-import java.io.IOException;
-import java.util.Objects;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public abstract class DeleteUser {
   /**
-   * Function to delete a directory.
-   * The function first check if the file is a directory
-   * Then delete it if the directory is empty
-   * If it is not delete, list all the directory content
-   * Then delete all the content
+   * Remove user from the lo23-users.ser file.
+   * write every user in a temp file
+   * skip the user we want to delete
+   * save the new temp file as our old one
    *
-   * @param file File
+   * @param filePath Path
+   * @param username String
    */
-  private static void deleteDirectory(File file) throws IOException {
-    if (file.isDirectory()) {
+  private static void deleteUserFromDisk(Path filePath, String username
+                                         ) throws Exception {
+    FileInputStream file = new FileInputStream(filePath.toFile());
+    ObjectInputStream reader = new ObjectInputStream(file);
+    File temp = new File("_temp_");
+    FileOutputStream tempFile = new FileOutputStream(temp);
+    ObjectOutputStream writer = new ObjectOutputStream(tempFile);
+    LocalUser user;
 
-      //directory is empty, then delete it
-      if (file.list().length == 0) {
-        file.delete();
-        System.out.println("Directory is deleted : "
-            + file.getAbsolutePath());
-
-      } else {
-        //list all the directory contents
-        String[] files = file.list();
-
-        for (String temp : files) {
-          //construct the file structure
-          File fileDelete = new File(file, temp);
-
-          //recursive delete
-          deleteDirectory(fileDelete);
-        }
-
-        //check the directory again, if empty then delete it
-        if (Objects.requireNonNull(file.list()).length == 0) {
-          file.delete();
-          System.out.println("Directory is deleted : "
-              + file.getAbsolutePath());
-        }
-      }
-
-    } else {
-      //if file, then delete it
-      file.delete();
-      System.out.println("File is deleted : " + file.getAbsolutePath());
+    //Write the first part of the file
+    try {
+      do {
+        user = (LocalUser) reader.readObject();
+        writer.writeObject(user);
+      } while (!(user.getUsername().equals(username)));
+    } catch (ClassNotFoundException e) {
+      throw new Exception("Local save may be corrupted or outdated");
     }
+    //Skip the user we want to delete
+    writer.writeObject(null);
+
+    //Write the second part of the file
+    try {
+      do {
+        user = (LocalUser) reader.readObject();
+        writer.writeObject(user);
+      } while (!(user.getUsername().equals(username)));
+    } catch (EOFException e) {
+      //Replace old file with the new one
+      temp.renameTo(filePath.toFile()); //Not executed ?
+    } catch (ClassNotFoundException e) {
+      throw new Exception("Local save may be corrupted or outdated");
+    }
+
   }
 
   /**
    * Delete an account.
-   * Delete the directory link to the user we
-   * want to delete
+   * Delete the properties file linked to the user we want to delete
+   * Remove user from the lo23-users.ser file.
    * Then delete LocalUser object by giving it the value null
    *
    * @param dc Datacore
    */
-  public static void run(Datacore dc) {
-    File directoryToDelete = new File(dc.getCurrentUser().getSavePath().toString());
-
-    if (!directoryToDelete.exists()) {
-      System.out.println("Directory does not exist.");
+  public static void run(Datacore dc) throws Exception {
+    LocalUser user = dc.getCurrentUser();
+    File propFileToDelete = new File(user.getSavePath().resolve(user.getUsername()
+        + "-config.properties").toString());
+    if (!propFileToDelete.exists()) {
+      System.out.println("Properties file for this user does not exist. Can't be deleted");
     } else {
-      try {
-        deleteDirectory(directoryToDelete);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+      propFileToDelete.delete();
+      System.out.println("Properties file deleted");
     }
+
+    Path savePath = Paths.get("").toAbsolutePath();
+    deleteUserFromDisk(savePath.resolve(dc.LOCAL_USERS_FILENAME), user.getUsername());
 
     dc.setCurrentUser(null);
   }
