d0f8fdb21b9b062f75b064d6a8a33bddb1504cdb
Alexandre Touzeau <alexandre.touzeau@etu.utc.fr>
2019-11-19 17:40:31 +0100

    Inverse order for delete LocalUser in lo23-users.ser and remove properties

--- data/src/main/java/features/DeleteUser.java
+++ data/src/main/java/features/DeleteUser.java
@@ -10,27 +10,28 @@ import java.io.IOException;
 public abstract class DeleteUser {
   /**
    * Delete an account.
-   * Delete the properties file linked to the user we want to delete
    * Remove user from the lo23-users.ser file.
+   * Delete the properties file linked to the user we want to delete.
    * Wipe all useless data and set current user to null
    *
    * @param dc Datacore
    */
   public static void run(Datacore dc) throws IOException {
     LocalUser user = dc.getCurrentUser();
-    File propFileToDelete = new File(user.getSavePath().resolve(user.getUsername()
-        + "-config.properties").toString());
+    try {
+      dc.getLocalUsersFileHandler().remove(user);
+    } catch (LocalUsersFileException e) {
+      throw new IOException(e);
+    }
+
+    File propFileToDelete = user.getSavePath().resolve(user.getUsername()
+        + "-config.properties").toFile();
     if (propFileToDelete.exists() && propFileToDelete.delete()) {
       System.out.println("Properties file deleted");
     } else {
       System.out.println("Properties file for this user does not exist. Can't be deleted");
     }
 
-    try {
-      dc.getLocalUsersFileHandler().remove(user);
-    } catch (LocalUsersFileException e) {
-      throw new IOException(e);
-    }
     dc.wipe();
   }
 
