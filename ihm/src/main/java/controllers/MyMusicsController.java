2a546bb56a68bba08de0e461cfb05c1909a6128a
Paul Constant <paul.constant@etu.utc.fr>
2019-11-19 18:13:03 +0100

    Show musics in myMusicsController

--- ihm/src/main/java/controllers/MyMusicsController.java
+++ ihm/src/main/java/controllers/MyMusicsController.java
@@ -1,13 +1,29 @@
 package controllers;
 
+import core.IhmCore;
+import datamodel.MusicMetadata;
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 //replace by javadocs
 //central view with all user music
 public class MyMusicsController implements Controller {
 
   private NewMusicController newMusicController;
   private SearchMusicController searchMusicController;
-
   private CentralFrameController centralFrameController;
+  private IhmCore ihmCore;
+
+  @FXML private TableView<MusicMetadata> tvMusics;
+  @FXML private TableColumn<MusicMetadata, String> artistCol;
+  @FXML private TableColumn<MusicMetadata, String> titleCol;
+  @FXML private TableColumn<MusicMetadata, String> albumCol;
+  @FXML private TableColumn<MusicMetadata, Duration> durationCol;
 
   @Override
   public void initialize() {
@@ -36,5 +52,33 @@ public class MyMusicsController implements Controller {
 
   public void setCentralFrameController(CentralFrameController centralFrameController) {
     this.centralFrameController = centralFrameController;
+  }
+
+  public void setIhmCore(IhmCore ihmCore){
+    this.ihmCore = ihmCore;
+  }
+  /**
+   * Setup the table columns to receive data.
+   */
+  public void init() {
+
+    // "artist", "title", "album", "duration" refer to MusicMetaData attributes
+    this.artistCol.setCellValueFactory(new PropertyValueFactory<MusicMetadata, String>("artist"));
+    this.titleCol.setCellValueFactory(new PropertyValueFactory<MusicMetadata, String>("title"));
+    this.albumCol.setCellValueFactory(new PropertyValueFactory<MusicMetadata, String>("album"));
+    this.durationCol.setCellValueFactory(
+        new PropertyValueFactory<MusicMetadata, Duration>("duration"));
+
+    this.displayAvailableMusics();
+  }
+
+  public void displayAvailableMusics() {
+    tvMusics.getItems().setAll(this.parseMusic());
+  }
+
+  private List<MusicMetadata> parseMusic() {
+    return this.ihmCore.getDataForIhm().getAvailableMusics()
+        .map(x -> x.getMetadata())
+        .collect(Collectors.toList());
   }
 }
