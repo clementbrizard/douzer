504527734c7cab3ef5c423559892033ba26b3b8f
kchartra <kyllian.chartrain@etu.utc.fr>
2019-11-20 14:20:33 +0100

    fix: javadocs for CentralFrameController

--- ihm/src/main/java/controllers/CentralFrameController.java
+++ ihm/src/main/java/controllers/CentralFrameController.java
@@ -2,8 +2,15 @@ package controllers;
 
 import javafx.fxml.FXML;
 
-//replace by javadocs
-//the central view who will change often between centralview
+
+/**
+ * the central view who will change often with each subView and their Controllers.
+ * @see MyMusicsController
+ * @see DistantUserController
+ * @see ProfileEditController
+ * @see DetailsMusicController
+ * @see AllMusicsController
+ */
 public class CentralFrameController implements Controller {
 
   @FXML
@@ -15,64 +22,125 @@ public class CentralFrameController implements Controller {
   private DetailsMusicController detailsMusicController;
   private MainController mainController;
 
-  @Override
-  public void initialize() {
-  }
 
   // Getters
 
+  /**
+   * getter of allMusicsController the controller of the sub-view AllMusicsCenterView.
+   * @return AllMusicsController
+   * @see AllMusicsController
+   */
   public AllMusicsController getAllMusicsController() {
     return allMusicsController;
   }
 
+  /**
+   * getter of myMusicsController the controller of the sub-view MyMusicsCenterView.
+   * @return MyMusicsController
+   * @see MyMusicsController
+   */
   public MyMusicsController getMyMusicsController() {
     return myMusicsController;
   }
 
+  /**
+   * getter of distantUserController the controller of the sub-view OnlineUsersListView.
+   * @return DistantUserController
+   * @see DistantUserController
+   */
   public DistantUserController getDistantUserController() {
     return distantUserController;
   }
 
+  /**
+   * getter of profileEditController the controller of the sub-view ProfileEditView.
+   * @return ProfileEditController
+   * @see ProfileEditController
+   */
   public ProfileEditController getProfileEditController() {
     return profileEditController;
   }
 
+  /**
+   * getter of detailsMusicController the controller of the sub-view DetailsMusicView.
+   * @return DetailsMusicController
+   * @see DetailsMusicController
+   */
   public DetailsMusicController getDetailsMusicController() {
-    return this.detailsMusicController;
+    return detailsMusicController;
   }
 
+  /**
+   * getter of mainController the controller of the super-view MainView.
+   * @return MainController
+   * @see MainController
+   */
   public MainController getMainController() {
-    return this.mainController;
+    return mainController;
   }
 
   // Setters
 
+  /**
+   * setter of allMusicsController the controller of the sub-view AllMusicsCenterView.
+   * @param allMusicsController the new AllMusicsController
+   * @see AllMusicsController
+   */
   public void setAllMusicsController(AllMusicsController allMusicsController) {
     this.allMusicsController = allMusicsController;
   }
 
+  /**
+   * setter of myMusicsController the controller of the sub-view MyMusicsCenterView.
+   * @param myMusicsController the new MyMusicsController
+   * @see MyMusicsController
+   */
   public void setMyMusicsController(MyMusicsController myMusicsController) {
     this.myMusicsController = myMusicsController;
   }
 
+  /**
+   * setter of distantUserController the controller of the sub-view OnlineUsersListView.
+   * @param distantUserController the new DistantUserController
+   * @see DistantUserController
+   */
   public void setDistantUserController(DistantUserController distantUserController) {
     this.distantUserController = distantUserController;
   }
 
+  /**
+   * setter of allMusicsController the controller of the sub-view AllMusicsCenterView.
+   * @param profileEditController the new ProfileEditController
+   * @see ProfileEditController
+   */
   public void setProfileEditController(ProfileEditController profileEditController) {
     this.profileEditController = profileEditController;
   }
 
+  /**
+   * setter of detailsMusicController the controller of the sub-view DetailsMusicView.
+   * @param detailsMusicController the new DetailsMusicController
+   * @see DetailsMusicController
+   */
   public void setDetailsMusicController(DetailsMusicController detailsMusicController) {
     this.detailsMusicController = detailsMusicController;
   }
 
+  /**
+   * setter of mainController the controller of the super-view MainView.
+   * @param mainController the new MainController
+   * @see MainController
+   */
   public void setMainController(MainController mainController) {
     this.mainController = mainController;
   }
 
   // Other methods
-
+  
+  @Override
+  public void initialize() {
+  }
+  
   /**
    * Initialize the controllers inside the central frame.
    */
