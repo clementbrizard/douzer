6971862f35e5acffb8f55f137332bd83bb152801
Paul Constant <paul.constant@etu.utc.fr>
2019-11-25 16:49:33 +0100

    feat: show my musics

--- ihm/src/main/java/controllers/MyMusicsController.java
+++ ihm/src/main/java/controllers/MyMusicsController.java
@@ -1,8 +1,29 @@
 package controllers;
 
+import datamodel.MusicMetadata;
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 //replace by javadocs
 //central view with all user music
 public class MyMusicsController implements Controller {
+
+  @FXML
+  private TableView<MusicMetadata> tvMusics;
+  @FXML
+  private TableColumn<MusicMetadata, String> artistCol;
+  @FXML
+  private TableColumn<MusicMetadata, String> titleCol;
+  @FXML
+  private TableColumn<MusicMetadata, String> albumCol;
+  @FXML
+  private TableColumn<MusicMetadata, Duration> durationCol;
 
   private NewMusicController newMusicController;
   private SearchMusicController searchMusicController;
@@ -37,4 +58,42 @@ public class MyMusicsController implements Controller {
   public void setCentralFrameController(CentralFrameController centralFrameController) {
     this.centralFrameController = centralFrameController;
   }
+
+  /**
+   * Setup the table columns to receive data,
+   * this method has to be called right after the creation of the view.
+   */
+  public void init() {
+
+    // "artist", "title", "album", "duration" refer to MusicMetaData attributes
+    this.artistCol.setCellValueFactory(new PropertyValueFactory<MusicMetadata, String>("artist"));
+    this.titleCol.setCellValueFactory(new PropertyValueFactory<MusicMetadata, String>("title"));
+    this.albumCol.setCellValueFactory(new PropertyValueFactory<MusicMetadata, String>("album"));
+    this.durationCol.setCellValueFactory(
+        new PropertyValueFactory<MusicMetadata, Duration>("duration")
+    );
+
+    try {
+      this.displayAvailableMusics();
+    } catch (UnsupportedOperationException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  /**
+   * Refreshes the table with getLocalMusics() from DataForIhm
+   */
+  public void displayAvailableMusics() {
+    tvMusics.getItems().setAll(this.parseMusic());
+  }
+
+
+  private List<MusicMetadata> parseMusic() {
+    return this.getCentralFrameController().getMainController().getApplication()
+        .getIhmCore().getDataForIhm().getLocalMusics()
+        .map(x -> x.getMetadata())
+        .collect(Collectors.toList());
+  }
+
 }
