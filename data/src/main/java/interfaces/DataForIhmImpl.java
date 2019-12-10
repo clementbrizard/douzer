517ec1e534d1ef28ac41a690c50285afdd617b10
Maxime Meyer <maxime.meyer2@gmail.com>
2019-12-10 17:59:53 +0100

    fix: style

--- data/src/main/java/interfaces/DataForIhmImpl.java
+++ data/src/main/java/interfaces/DataForIhmImpl.java
@@ -169,7 +169,7 @@ public class DataForIhmImpl implements DataForIhm {
       dc.getLocalUsersFileHandler().add(localUser);
     }
 
-    if(userNameAlreadyExists){
+    if (userNameAlreadyExists) {
       throw new DataException("The user already exists.");
     }
   }
