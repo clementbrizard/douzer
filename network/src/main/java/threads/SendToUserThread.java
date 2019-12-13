d57f5187992ac8a6dc03824f4075a4cf7358ad6c
Antoine Pilon <antoine.pilon@etu.utc.fr>
2019-12-13 18:12:48 +0100

    fix: fix issue when  we send message to ip of a user not connected yet

--- network/src/main/java/threads/SendToUserThread.java
+++ network/src/main/java/threads/SendToUserThread.java
@@ -5,6 +5,7 @@ import java.io.OutputStream;
 import java.io.Serializable;
 import java.net.ConnectException;
 import java.net.InetAddress;
+import java.net.NoRouteToHostException;
 import java.net.Socket;
 
 import message.Message;
@@ -42,6 +43,8 @@ public class SendToUserThread extends ThreadExtend {
       socket.close();
     } catch (ConnectException e) {
       logger.error("Unable to send message. Is receiver listening ?");
+    } catch (NoRouteToHostException e) {
+      logger.error("Could not join distant user. It might not be connected yet.");
     } catch (Exception e) {
       logger.error(e);
       e.printStackTrace();
