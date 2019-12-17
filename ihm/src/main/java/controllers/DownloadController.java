b17ebd79c48fbfa8d161207a7df62dc04f0ba80c
Antoine Pilon <antoine.pilon@etu.utc.fr>
2019-12-17 19:25:11 +0100

    fix: fix download bug where user can't see download file

--- ihm/src/main/java/controllers/DownloadController.java
+++ ihm/src/main/java/controllers/DownloadController.java
@@ -4,6 +4,7 @@ import datamodel.Music;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.application.Platform;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.controlsfx.control.Notifications;
@@ -77,43 +78,51 @@ public class DownloadController implements Controller {
    * @param downloadProgress The downloaded music download progress
    */
   public void updateDownloadProgressBar(Music downloadedMusic, int downloadProgress) {
-    String downloadedMusicArtist = downloadedMusic.getMetadata().getArtist();
-    String downloadedMusicTitle = downloadedMusic.getMetadata().getTitle();
-    //A negative progress send by data means a download failure
-    if (downloadProgress < 0) {
-      Notifications.create()
-          .title("Download failed")
-          .text(downloadedMusicTitle + " - " + downloadedMusicArtist)
-          .darkStyle()
-          .showWarning();
-      // Clear download information label
-      this.lblDownload.setText("");
-      // Make download progress bar invisible
-      this.progressDownload.setVisible(false);
-    } else {
-      // Make download progress bar visible
-      this.progressDownload.setVisible(true);
-      this.progressDownload.setProgress(downloadProgress / 100);
-      this.lblDownload
-          .setText("Downloading : " + downloadedMusicTitle + " - " + downloadedMusicArtist);
-      // We update central views when download is done
-      if (downloadProgress == 100) {
-        Notifications.create()
-            .title("Download done")
-            .text(downloadedMusicTitle + " - " + downloadedMusicArtist)
-            .darkStyle()
-            .showInformation();
-        this.mainController
-            .getCentralFrameController()
-            .getAllMusicsController()
-            .init();
-        // Reinitialize progress bar with 0
-        this.progressDownload.setProgress(0);
-        // Clear download information label
-        this.lblDownload.setText("");
-        // Make download progress bar invisible
-        this.progressDownload.setVisible(false);
+    Platform.runLater(new Runnable() {
+      
+      @Override
+      public void run() {
+        String downloadedMusicArtist = downloadedMusic.getMetadata().getArtist();
+        String downloadedMusicTitle = downloadedMusic.getMetadata().getTitle();
+        //A negative progress send by data means a download failure
+        if (downloadProgress < 0) {
+          Notifications.create()
+              .title("Download failed")
+              .text(downloadedMusicTitle + " - " + downloadedMusicArtist)
+              .darkStyle()
+              .showWarning();
+          // Clear download information label
+          lblDownload.setText("");
+          // Make download progress bar invisible
+          progressDownload.setVisible(false);
+        } else {
+          // Make download progress bar visible
+          progressDownload.setVisible(true);
+          progressDownload.setProgress((float)downloadProgress / 100);
+          System.out.println((float)downloadProgress / 100);
+          lblDownload
+              .setText("Downloading : " + downloadedMusicTitle + " - " + downloadedMusicArtist);
+          // We update central views when download is done
+          if (downloadProgress == 100) {
+            Notifications.create()
+                .title("Download done")
+                .text(downloadedMusicTitle + " - " + downloadedMusicArtist)
+                .darkStyle()
+                .showInformation();
+            mainController
+                .getCentralFrameController()
+                .getAllMusicsController()
+                .init();
+            // Reinitialize progress bar with 0
+            progressDownload.setProgress(0);
+            // Clear download information label
+            lblDownload.setText("");
+            // Make download progress bar invisible
+            progressDownload.setVisible(false);
+            getMainController().getCentralFrameController().getMyMusicsController().displayAvailableMusics();
+          }
+        }
       }
-    }
+    });
   }
 }
