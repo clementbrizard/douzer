bb40d10ea693eef77b4ad22bf18db5cd7c6dcf4c
Maxime Meyer <maxime.meyer2@gmail.com>
2019-12-17 18:34:01 +0100

    fix: imports

--- data/src/main/java/core/LocalUsersFileHandler.java
+++ data/src/main/java/core/LocalUsersFileHandler.java
@@ -1,23 +1,16 @@
 package core;
 
-import datamodel.LocalMusic;
 import datamodel.LocalUser;
-
 import java.io.EOFException;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import javax.imageio.ImageIO;
-
 
 /**
  * This class allows to access a LocalUsersFile.
