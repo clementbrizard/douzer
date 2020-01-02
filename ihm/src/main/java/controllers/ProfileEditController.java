34309e90ae2700d9da069c1f45bdfebf0db4d894
aberanger <aurelien.beranger@lilo.org>
2020-01-02 18:08:45 +0100

    fix: adapt avatar management in ProfileEdit with new FormatImage functions in utils

--- ihm/src/main/java/controllers/ProfileEditController.java
+++ ihm/src/main/java/controllers/ProfileEditController.java
@@ -3,13 +3,10 @@ package controllers;
 import static impl.org.controlsfx.tools.MathTools.min;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
 import java.awt.image.RenderedImage;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.nio.file.Path;
 import java.time.LocalDate;
-import java.util.Hashtable;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
@@ -32,6 +29,7 @@ import javax.imageio.ImageIO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.controlsfx.control.Notifications;
+import utils.FormatImage;
 
 /**
  *  Central view that permit the user to edit his profile.
@@ -157,33 +155,6 @@ public class ProfileEditController implements Controller {
     datePickerBirth.setPromptText(birthDate.toString());
     datePickerBirth.setValue(birthDate);
   }
-  
-  /**
-   * this function permit to convert a RenderedImage into a BufferedImage
-   * I took this function here http://www.jguru.com/faq/view.jsp?EID=114602.
-   * @param img a RenderedImage
-   * @return a BufferedImage
-   */
-  private BufferedImage convertRenderedImage(RenderedImage img) {
-    if (img instanceof BufferedImage) {
-      return (BufferedImage)img;  
-    }   
-    ColorModel cm = img.getColorModel();
-    int width = img.getWidth();
-    int height = img.getHeight();
-    WritableRaster raster = cm.createCompatibleWritableRaster(width, height);
-    boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
-    Hashtable properties = new Hashtable();
-    String[] keys = img.getPropertyNames();
-    if (keys != null) {
-      for (int i = 0; i < keys.length; i++) {
-        properties.put(keys[i], img.getProperty(keys[i]));
-      }
-    }
-    BufferedImage result = new BufferedImage(cm, raster, isAlphaPremultiplied, properties);
-    img.copyData(raster);
-    return result;
-  }
 
   /**
    * this function show the avatar image.
@@ -198,32 +169,8 @@ public class ProfileEditController implements Controller {
             .getCurrentUser()
             .getAvatar();
 
-    // thanks to SwingFXUtils we can convert a bufferedImage into an Image
-    Image img = SwingFXUtils.toFXImage(convertRenderedImage(imgRend),null);
-    imgAvatar.setImage(img);
-
-    // crop of the image to make square
-    double minSide = min(img.getHeight(), img.getWidth());
-    PixelReader imgReader = img.getPixelReader();
-
-    // if the image isn't square, we crop it
-    if (img.getHeight() != img.getWidth()) {
-      if (img.getHeight() < img.getWidth()) {
-        // define crop in image coordinates:
-        double x = (img.getWidth() - img.getHeight()) / 2;
-        double y = 0;
-        // define the square for cropping
-        Rectangle2D croppedPortion = new Rectangle2D(x, y, minSide, minSide);
-        imgAvatar.setViewport(croppedPortion);
-      } else if (img.getHeight() > img.getWidth()) {
-        // define crop in image coordinates:
-        double x = 0;
-        double y = (img.getHeight() - img.getWidth()) / 2;
-        // define the square for cropping
-        Rectangle2D croppedPortion = new Rectangle2D(x, y, minSide, minSide);
-        imgAvatar.setViewport(croppedPortion);
-      }
-    }
+    // Convert and crop the avatar image
+    FormatImage.cropAvatar(imgRend, imgAvatar);
 
     double circleRadius = 45;
     paneImgAvatar.setMinWidth(circleRadius * 2);
