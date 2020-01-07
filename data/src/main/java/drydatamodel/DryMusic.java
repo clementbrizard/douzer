4b1278d4ff47272301b3229b97178c8df169a812
efugier <mail@emilienfugier.net>
2020-01-07 19:27:06 +0100

    refactor more control on collected sets

--- data/src/main/java/drydatamodel/DryMusic.java
+++ data/src/main/java/drydatamodel/DryMusic.java
@@ -3,6 +3,7 @@ package drydatamodel;
 import core.Datacore;
 import datamodel.Music;
 import datamodel.User;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -15,12 +16,12 @@ public class DryMusic implements java.io.Serializable {
   public DryMusic(Music music) {
     this.music = new Music(music);
     this.metadata = new DryMusicMetadata(music.getMetadata());
-    this.owners = music.getOwners().stream().map(User::getUuid).collect(Collectors.toSet());
+    this.owners = music.getOwners().stream().map(User::getUuid).collect(Collectors.toCollection(HashSet::new));
   }
 
   public Music hydrate(Datacore dc) {
     this.music.setMetadata(this.metadata.hydrate(dc));
-    this.music.setOwners(owners.stream().map(dc::getUser).collect(Collectors.toSet()));
+    this.music.setOwners(owners.stream().map(dc::getUser).collect(Collectors.toCollection(HashSet::new)));
     return this.music;
   }
 }
