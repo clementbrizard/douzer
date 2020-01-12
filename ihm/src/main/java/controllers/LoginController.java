da838c2374a7fa71f675274edeffbb086806d97a
cbrizard <clement.brizard@etu.utc.fr>
2020-01-12 02:02:18 +0100

    fix: put cursor in pseudo when log out

--- ihm/src/main/java/controllers/LoginController.java
+++ ihm/src/main/java/controllers/LoginController.java
@@ -158,5 +158,6 @@ public class LoginController implements Controller {
     // Clear all text fields
     textFieldPassword.setText("");
     textFieldPseudo.setText("");
+    textFieldPseudo.requestFocus();
   }
 }
